4a069f8d960f3e8f5683a925ed5aa76f33021df1
Fernando Rodríguez <frodrig76@gmail.com>
2013-02-26 09:38:17 +0100

    Update de la web

--- Web/public_html/Easy_Incomes_and_Expenses/Easy_Incomes_and_Expenses_-_English_files/Easy_Incomes_and_Expenses_-_English.js
+++ Web/public_html/Easy_Incomes_and_Expenses/Easy_Incomes_and_Expenses_-_English_files/Easy_Incomes_and_Expenses_-_English.js
@@ -1,9 +1,9 @@
-// Created by iWeb 3.0.4 local-build-20130212
+// Created by iWeb 3.0.4 local-build-20130219
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('Easy_Incomes_and_Expenses_-_English_files/Easy_Incomes_and_Expenses_-_EnglishMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');adjustLineHeightIfTooBig('id9');adjustFontSizeIfTooBig('id9');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');adjustLineHeightIfTooBig('id8');adjustFontSizeIfTooBig('id8');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}

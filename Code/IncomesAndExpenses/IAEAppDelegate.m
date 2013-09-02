3d4cadb7731e117353ec55ce2b800fb3dbcf4f41
Fernando Rodríguez <frodrig76@gmail.com>
2013-09-02 19:05:03 +0200

    Estado de la aplicación: * Guardamos en Terminate

--- Code/IncomesAndExpenses/IAEAppDelegate.m
+++ Code/IncomesAndExpenses/IAEAppDelegate.m
@@ -135,7 +135,6 @@ static NSString * const kUserDefaultsDayModeActiveKey = @"dayModeActive";
 {
     // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    [[IAEBook sharedBook] saveAll];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
@@ -152,6 +151,7 @@ static NSString * const kUserDefaultsDayModeActiveKey = @"dayModeActive";
 {
     // Saves changes in the application's managed object context before the application terminates.
     //[self saveContext];
+    [[IAEBook sharedBook] saveAll];
 }
 
 

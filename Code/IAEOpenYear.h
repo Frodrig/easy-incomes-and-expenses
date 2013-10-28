58b1cfd96a4a1317ba12532d9073e6bb23eddaee
Fernando Rodríguez <frodrig76@gmail.com>
2013-10-28 18:15:38 +0100

    Creada la clase IAEYearOpen sin implementación alguna

---
+++ Code/IAEOpenYear.h
@@ -0,0 +1,14 @@
+//
+//  IAEOpenYear.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez on 28/10/13.
+//  Copyright (c) 2013 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "IAEYearObject.h"
+
+@interface IAEOpenYear : NSObject<IAEYearObject>
+
+@end
---
+++ Code/IAEOpenYear.m
@@ -0,0 +1,83 @@
+//
+//  IAEOpenYear.m
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez on 28/10/13.
+//  Copyright (c) 2013 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import "IAEOpenYear.h"
+
+@implementation IAEOpenYear
+
+
+- (NSDecimalNumber *)expenses
+{
+    return nil;
+}
+
+- (NSDecimalNumber *)incomes
+{
+    return nil;
+}
+
+- (NSDecimalNumber *)balance
+{
+    return nil;
+}
+
+- (NSDecimalNumber *)balanceOfAllConceptsOfCategory:(IAECategory *)category
+{
+    return nil;
+}
+
+- (void)beginCategoryConceptSearchMode
+{
+}
+
+- (void)endCategoryConceptSearchMode
+{
+}
+
+- (NSArray *)findAllOrdererMonthsWithConcepts
+{
+    return nil;
+}
+
+- (NSArray *)findAllConceptsWithCategory:(IAECategory *)category
+{
+    return nil;
+}
+
+- (NSArray *)findAllConcepts
+{
+    return nil;
+}
+
+- (NSArray *)findAllConceptsSortedByEntryInstant
+{
+    return nil;
+}
+
+- (NSArray *)findAllConceptsSortedByDay
+{
+    return nil;
+}
+
+- (NSArray *)findAllCategoriesSortedByAbsoluteValueOfAmountsInConceptsOfType:(CategoryType)type
+{
+    return nil;
+}
+
+- (NSUInteger)findNumberOfConcepts
+{
+    return 0;
+}
+
+- (NSString *)yearDateAsString
+{
+    return nil;
+}
+
+
+@end

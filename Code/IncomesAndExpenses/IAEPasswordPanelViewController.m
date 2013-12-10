6a05172db6f817c2846de05e39f3fc5242b958b1
Fernando Rodríguez <frodrig76@gmail.com>
2013-12-10 14:08:48 +0100

    Creamos los paneles de introducción de contraseña acorde sea la acción a realizar. Problemas con la dimensión del scrollview y del panel interior. El scrollview parece ser más grande de lo que debería.

--- Code/IncomesAndExpenses/IAEPasswordPanelViewController.m
+++ Code/IncomesAndExpenses/IAEPasswordPanelViewController.m
@@ -21,6 +21,10 @@
 @implementation IAEPasswordPanelViewController
 
 
+#pragma mark - constants
+
+static const NSInteger kBasePanelPasswordTag = 100;
+
 #pragma mark - Init
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
@@ -48,25 +52,13 @@
     [super viewDidLoad];
     
     [self configureNavigationController];
+    [self configureScrollViewBasedInMode];
 }
 
 - (void)configureNavigationController
 {
     NSString *title = [self findTitleForNavigationControllerWithModeType:self.mode];
     self.navigationItem.title = title;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    
-    [self configureScrollViewBasedInMode];
-}
-
-- (void)configureScrollViewBasedInMode
-{
-    IAEPasswordPanelScreenView *view = (IAEPasswordPanelScreenView *)[UIView viewFromXib:@"IAEPasswordPanelScreenView" withOwner:self];
-    [self.scrollView addSubview:view];
 }
 
 -(NSString *)findTitleForNavigationControllerWithModeType:(ModeType)mode
@@ -85,5 +77,68 @@
     
     return retTitle;
 }
+
+- (void)configureScrollViewBasedInMode
+{
+    if (self.mode == MT_Activate) {
+        [self addCreatePasswordPanelScreen];
+    } else if (self.mode == MT_Deactivate) {
+        [self addInsertToValidatePasswordPanelScreen];
+    } else if (self.mode == MT_Change) {
+        [self addInsertToChangePasswordPanelScreen];
+    }
+}
+
+- (void)addCreatePasswordPanelScreen
+{
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.bounds.size.width * 2, self.scrollView.bounds.size.height);
+    
+    [self createAndInsertPasswordPanelScreenViewWithMessage:@"LTEXT_PASSWORDPANEL_SCREENMESSAGE_INSERTOCREATE" atScrollViewPosition:0];
+    [self createAndInsertPasswordPanelScreenViewWithMessage:@"LTEXT_PASSWORDPANEL_SCREENMESSAGE_INSERTOCONFIRM" atScrollViewPosition:1];
+}
+
+- (void)addInsertToValidatePasswordPanelScreen
+{
+    [self createAndInsertPasswordPanelScreenViewWithMessage:@"LTEXT_PASSWORDPANEL_SCREENMESSAGE_INSERTOVALIDATE" atScrollViewPosition:0];
+}
+
+- (void)addInsertToChangePasswordPanelScreen
+{
+    self.scrollView.contentSize = CGSizeMake(self.scrollView.bounds.size.width * 3, self.scrollView.bounds.size.height);
+
+    [self createAndInsertPasswordPanelScreenViewWithMessage:@"LTEXT_PASSWORDPANEL_SCREENMESSAGE_INSERTOCHANGE" atScrollViewPosition:0];
+    [self createAndInsertPasswordPanelScreenViewWithMessage:@"LTEXT_PASSWORDPANEL_SCREENMESSAGE_INSERTOCREATE" atScrollViewPosition:1];
+    [self createAndInsertPasswordPanelScreenViewWithMessage:@"LTEXT_PASSWORDPANEL_SCREENMESSAGE_INSERTOCONFIRM" atScrollViewPosition:2];
+}
+
+- (void)createAndInsertPasswordPanelScreenViewWithMessage:(NSString *)message atScrollViewPosition:(NSInteger)position
+{
+    IAEPasswordPanelScreenView *panel = (IAEPasswordPanelScreenView *)[UIView viewFromXib:@"IAEPasswordPanelScreenView" withOwner:self];
+    panel.frame = CGRectMake(panel.bounds.size.width * position, 0, panel.bounds.size.width, panel.bounds.size.height);
+    panel.tag = [self createTagForPasswordPanelAtPosition:position];
+    [panel setMessage:NSLocalizedString(message, @"")];
+    
+    [self.scrollView addSubview:panel];
+}
+
+- (IAEPasswordPanelScreenView *)findPasswordPanelScreenAtPosition:(NSInteger)position
+{
+    const NSInteger tag = [self createTagForPasswordPanelAtPosition:position];
+    IAEPasswordPanelScreenView *panel = (IAEPasswordPanelScreenView *)[self.scrollView viewWithTag:tag];
+    
+    return panel;
+}
+
+- (NSInteger)createTagForPasswordPanelAtPosition:(NSInteger)position
+{
+    const NSInteger tag = kBasePanelPasswordTag + position;
+    
+    return tag;
+}
+
+
+
+
+
 
 @end

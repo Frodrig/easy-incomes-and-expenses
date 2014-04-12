dc106049cdd6666e07f7c05fb893012d61d5dcf2
Fernando Rodriguez Martinez <frodrig76@gmail.com>
2014-04-12 12:55:42 +0200

    Clases MonthSelectorView y Delegate RECIEN creadas

---
+++ Code/IncomesAndExpenses/IAEMonthSelectorView.h
@@ -0,0 +1,13 @@
+//
+//  IAEMonthSelectorView.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 12/04/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAEMonthSelectorView : UIView
+
+@end
---
+++ Code/IncomesAndExpenses/IAEMonthSelectorView.m
@@ -0,0 +1,31 @@
+//
+//  IAEMonthSelectorView.m
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 12/04/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import "IAEMonthSelectorView.h"
+
+@implementation IAEMonthSelectorView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end

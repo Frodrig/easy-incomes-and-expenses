31722375f6f2561c072231da9f87996f0365c24a
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-06 18:48:35 +0200

    WIP refactorización de la arquitectura para usar IAEEmailRequest como class cluster y IAEEmailRequestRecovery y IAEEmailRequestRecoveryMailLinked

--- Code/IncomesAndExpenses/IAEPasswordRecoveryEmailPanelViewController.m
+++ Code/IncomesAndExpenses/IAEPasswordRecoveryEmailPanelViewController.m
@@ -9,11 +9,7 @@
 #import "IAEPasswordRecoveryEmailPanelViewController.h"
 #import "NSUserDefaults+EasyIncAndExp.h"
 #import "IAEEmailChecker.h"
-#import <AWSRuntime/AWSRuntime.h>
-#import <AWSSES/AWSSES.h>
-
-static NSString * const kAWSAccessKeyId = @"REMOVED_AWS_KEY_ID";
-static NSString * const kAWSSecretKey = @"REMOVED_AWS_SECRET";
+#import "IAEEmailSender.h"
 
 @interface IAEPasswordRecoveryEmailPanelViewController ()<UITextFieldDelegate,
                                                           UIAlertViewDelegate>
@@ -152,38 +148,19 @@ static NSString * const kAWSSecretKey = @"REMOVED_AWS_SECRET";
 
 - (IBAction)saveButton:(id)sender
 {
-    //[self saveActualEmailRecoveryAddress];
+    [self saveActualEmailRecoveryAddress];
+    [self sendConfirmationLinkedEmailRecoveryAddress];
     [self launchPreDismissAlertViewWithInformationAboutVinculeEmailRecoveryAddress];
 }
 
 - (void)saveActualEmailRecoveryAddress
 {
     [[NSUserDefaults standardUserDefaults] vinculePasswordRecoveryEmail:[self actualPasswordRecoveryEmailWithValidFormat]];
-    
-     AmazonSESClient *sesClient = [[AmazonSESClient alloc] initWithAccessKey:kAWSAccessKeyId withSecretKey:kAWSSecretKey];
-     
-     SESContent *messageBody = [[SESContent alloc] init];
-     messageBody.data = @"Test enviado correo con Amazon SES";
-     
-     SESContent *subject = [[SESContent alloc] init];
-     subject.data = @"Title";
-    
-     SESBody *body = [[SESBody alloc] init];
-     body.text = messageBody;
-     
-     SESMessage *message = [[SESMessage alloc] init];
-     message.subject = subject;
-     message.body    = body;
-     
-     SESDestination *destination = [[SESDestination alloc] init];
-     [destination.toAddresses addObject:[self actualPasswordRecoveryEmailWithValidFormat]];
-     
-     SESSendEmailRequest *ser = [[SESSendEmailRequest alloc] init];
-     ser.source      = @"hola@frodrig.com";
-     ser.destination = destination;
-     ser.message     = message;
-     
-    SESSendEmailResponse *response = [sesClient sendEmail:ser];
+}
+
+- (void)sendConfirmationLinkedEmailRecoveryAddress
+{
+    [[IAEEmailSender sharedInstance] sendRecoveryPasswordEmailTo:[self actualPasswordRecoveryEmailWithValidFormat]];
 }
 
 - (void)launchPreDismissAlertViewWithInformationAboutVinculeEmailRecoveryAddress
---
+++ Code/IncomesAndExpenses/IAERecoveryMailLinkedEmailRequest.h
@@ -0,0 +1,16 @@
+//
+//  IAERecoveryMailLinkedEmailRequest.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 06/05/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import "IAEEmailRequest.h"
+
+@interface IAERecoveryMailLinkedEmailRequest : IAEEmailRequest
+
+- (NSString *)findTitleText;
+- (NSString *)findMessageText;
+
+@end

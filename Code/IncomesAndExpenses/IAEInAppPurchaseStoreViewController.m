0112cd967214055f1532f62a02384dc62f6a898c
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-16 09:48:04 +0200

    =ViewController to implement the In-App Purchased section. For now is clean of implementation. Only the starting point.

---
+++ Code/IncomesAndExpenses/IAEInAppPurchaseStoreViewController.m
@@ -0,0 +1,38 @@
+//
+//  IAEInAppPurchaseStoreViewController.m
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 16/05/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import "IAEInAppPurchaseStoreViewController.h"
+
+@interface IAEInAppPurchaseStoreViewController ()
+
+@end
+
+@implementation IAEInAppPurchaseStoreViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

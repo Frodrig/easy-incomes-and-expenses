3a4763ed9759ea61d63488fe4dffa22870f8009d
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-16 09:21:31 +0200

    Especializamos el label del navigation principal para poder capturar los eventos que en el se produzcan.

---
+++ Code/IncomesAndExpenses/IAEMainNavigationTitle.h
@@ -0,0 +1,13 @@
+//
+//  IAEMainNavigationTitle.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 16/05/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAEMainNavigationTitle : UILabel
+
+@end

c18fefd688ce14b136d44beff2546aa972d9783e
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-16 09:48:04 +0200

    =ViewController to implement the In-App Purchased section. For now is clean of implementation. Only the starting point.

---
+++ Code/IncomesAndExpenses/IAEInAppPurchaseStoreViewController.h
@@ -0,0 +1,13 @@
+//
+//  IAEInAppPurchaseStoreViewController.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 16/05/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAEInAppPurchaseStoreViewController : UIViewController
+
+@end

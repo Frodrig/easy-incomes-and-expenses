c29cf9fe64b3159165ffc3db9fbe2e4afe4d5ce7
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-16 19:10:16 +0200

    =Design of the purchase window done. Pending to link the UIControls and localize elements

--- Code/IncomesAndExpenses/IAEMainNavigationTitle.m
+++ Code/IncomesAndExpenses/IAEMainNavigationTitle.m
@@ -37,7 +37,8 @@ static NSString * const kNotificationMainLabelTitleTouched = @"mainLabelTitleTou
 
 - (void)configureText
 {
-    self.text = NSLocalizedString([[NSUserDefaults standardUserDefaults] isProVersionEnabled] ?@"LTEXT_MAINNAVIGATION_TITLE_PROVERSION" : @"LTEXT_MAINNAVIGATION_TITLE_NOPROVERSION" , @"");
+    [[NSUserDefaults standardUserDefaults] disableProVersion];
+    self.text = NSLocalizedString([[NSUserDefaults standardUserDefaults] isProVersionEnabled] ? @"LTEXT_MAINNAVIGATION_TITLE_PROVERSION" : @"LTEXT_MAINNAVIGATION_TITLE_NOPROVERSION" , @"");
 }
 
 

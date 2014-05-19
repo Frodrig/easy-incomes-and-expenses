e13cf1fa006652d66771f7f5ae3c99828b1bd567
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-19 10:22:56 +0200

    =Added an class utility to make NSString hashed value from a NSString identifier. Is a copy-paste of a method from Apple and a good starting point to convert a unique CFUUIDCreateString identifier to a hashed one for use with applicationUsername in a payment transaction

---
+++ Code/IncomesAndExpenses/IAEHasthUtilities.h
@@ -0,0 +1,15 @@
+//
+//  IAEHasthUtilities.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 19/05/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IAEHasthUtilities : NSObject
+
++ (NSString *)hashedValueForStringIdentifier:(NSString*)identifier;
+
+@end

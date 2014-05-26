e481f21cfa7f0a78d8554893b4fc78afcd9ddab2
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-26 11:43:25 +0200

    =Revision of the password module. Now when you tap a code number the * symbol will be more beautiful, centered and will appear with a fade fx

---
+++ Code/IncomesAndExpenses/IAEPasswordCheckView.h
@@ -0,0 +1,16 @@
+//
+//  IAEPasswordCheckView.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 26/05/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAEPasswordCheckView : UIView
+
+- (void)showCheckWithAnimation:(BOOL)animation;
+- (void)hideCheckWithAnimation:(BOOL)animation;
+
+@end

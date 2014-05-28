73a764ecfe398821feb197342775299464b87f68
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-05-28 18:16:51 +0200

    Se añade una animación muy sutil en el título que reclama la compra de la aplicación. La animación controla también si estamos en foreground o en background para pararla o activarla ya que por defecto el sistema no hace esa gestión.

--- Code/IncomesAndExpenses/IAEMainNavigationTitle.m
+++ Code/IncomesAndExpenses/IAEMainNavigationTitle.m
@@ -29,20 +29,73 @@ static NSString * const kNotificationMainLabelTitleTouched = @"mainLabelTitleTou
 {
     self = [super initWithCoder:aDecoder];
     if (self) {
-        [self configureText];
+        [self configureNotificationObservers];
+        [self configureTextAndAnimation];
     }
     
     return self;
+}
+- (void)configureNotificationObservers
+{
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationDidEnterBackground:)
+                                                 name:UIApplicationDidEnterBackgroundNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(applicationWillEnterForeground:)
+                                                 name:UIApplicationWillEnterForegroundNotification
+                                               object:nil];
+}
+
+- (void)configureTextAndAnimation
+{
+    [self configureText];
+    [self configureAnimation];
 }
 
 - (void)configureText
 {
     self.text = NSLocalizedString([[NSUserDefaults standardUserDefaults] isProVersionEnabled] ? @"LTEXT_MAINNAVIGATION_TITLE_PROVERSION" : @"LTEXT_MAINNAVIGATION_TITLE_NOPROVERSION" , @"");
+
+}
+
+- (void)configureAnimation
+{
+    if ([[NSUserDefaults standardUserDefaults] isProVersionEnabled]) {
+        [self removeAllAnimations];
+    } else {
+        [self addAnimations];
+    }
+}
+
+- (void)removeAllAnimations
+{
+    [self.layer removeAllAnimations];
+    self.alpha = 1.0;
+}
+
+- (void)addAnimations
+{
+    [self.layer addAnimation:[self createAlphaAnimation] forKey:@"proVersionMarketingAlphaAnimation"];
+}
+
+- (CABasicAnimation *)createAlphaAnimation
+{
+    CABasicAnimation *animationAlpha = [CABasicAnimation animationWithKeyPath:@"opacity"];
+    animationAlpha.fromValue = @(1.0);
+    animationAlpha.toValue = @(0.5);
+    animationAlpha.duration = 1.0;
+    animationAlpha.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    animationAlpha.autoreverses = YES;
+    animationAlpha.repeatCount = HUGE_VALF;
+
+    return animationAlpha;
 }
 
 - (void)reloadTitle
 {
-    [self configureText];
+    [self configureTextAndAnimation];
 }
 
 #pragma mark - Interaction
@@ -65,6 +118,18 @@ static NSString * const kNotificationMainLabelTitleTouched = @"mainLabelTitleTou
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event
 {
     
+}
+
+#pragma mark - Notification center
+
+- (void)applicationDidEnterBackground:(NSNotification *)notification
+{
+    [self removeAllAnimations];
+}
+
+- (void)applicationWillEnterForeground:(NSNotification *)notification
+{
+    [self configureAnimation];
 }
 
 @end

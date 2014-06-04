801ca48da90987cc2bf256fbedbaa65efc54ef58
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-06-04 16:46:10 +0200

    Esqueleto preliminar para navegar por pantallas ya cargadas gráficamente y correctamente situadas

--- Code/IncomesAndExpenses/IAEHelpCarouselViewController.m
+++ Code/IncomesAndExpenses/IAEHelpCarouselViewController.m
@@ -8,11 +8,30 @@
 
 #import "IAEHelpCarouselViewController.h"
 
+
+
 @interface IAEHelpCarouselViewController ()
+
+@property (weak, nonatomic) IBOutlet UIPageControl *pageController;
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, strong) NSArray *helpScreens;
 
 @end
 
 @implementation IAEHelpCarouselViewController
+
+#pragma mark - Properties
+
+- (NSArray *)helpScreens
+{
+    if (!_helpScreens) {
+        _helpScreens = @[@"HelpScreen_01.jpg", @"HelpScreen_01.jpg", @"HelpScreen_01.jpg"];
+    }
+    
+    return _helpScreens;
+}
+
+#pragma mark - Init
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -26,7 +45,49 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    [self prepareScrollView];
+    [self configurePageController];
+}
+
+
+- (void)prepareScrollView
+{
+    [self configureScrollView];
+    [self addImagesToScrollView];
+}
+
+- (void)configureScrollView
+{
+    self.scrollView.contentOffset = CGPointZero;
+    self.scrollView.contentSize = CGSizeMake(CGRectGetWidth(self.scrollView.frame) * self.helpScreens.count, CGRectGetHeight(self.scrollView.frame));
+    NSLog(@"SCROLL VIEW contentSize %@", NSStringFromCGSize(self.scrollView.contentSize));
+    self.scrollView.bounces = YES;
+    self.scrollView.showsHorizontalScrollIndicator = NO;
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    self.scrollView.pagingEnabled = YES;
+}
+
+- (void)addImagesToScrollView
+{
+    for (NSUInteger helpScreenIndexIt = 0; helpScreenIndexIt < self.helpScreens.count; ++helpScreenIndexIt) {
+        UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:self.helpScreens[helpScreenIndexIt]]];
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+        imageView.center = CGPointMake(CGRectGetMidX(self.scrollView.frame) + CGRectGetWidth(self.scrollView.frame) * helpScreenIndexIt, CGRectGetMidY(self.scrollView.frame));
+        NSLog(@"ImageView CENTER %@", NSStringFromCGPoint(imageView.center));
+        
+        [self.scrollView addSubview:imageView];
+    }
+}
+
+- (void)configurePageController
+{
+    
 }
 
 - (void)didReceiveMemoryWarning

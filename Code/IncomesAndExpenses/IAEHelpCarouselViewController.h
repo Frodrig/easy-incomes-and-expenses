ce0abe6936398500b7de3df7c500a47a2d76cbaf
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-06-04 15:36:05 +0200

    Versión preliminar del control donde alojar la ayuda

---
+++ Code/IncomesAndExpenses/IAEHelpCarouselViewController.h
@@ -0,0 +1,13 @@
+//
+//  IAEHelpCarouselViewController.h
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 04/06/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IAEHelpCarouselViewController : UIViewController
+
+@end
---
+++ Code/IncomesAndExpenses/IAEHelpCarouselViewController.m
@@ -0,0 +1,45 @@
+//
+//  IAEHelpCarouselViewController.m
+//  IncomesAndExpenses
+//
+//  Created by Fernando Rodríguez Martínez on 04/06/14.
+//  Copyright (c) 2014 Fernando Rodríguez Martínez. All rights reserved.
+//
+
+#import "IAEHelpCarouselViewController.h"
+
+@interface IAEHelpCarouselViewController ()
+
+@end
+
+@implementation IAEHelpCarouselViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Events
+
+
+- (IBAction)doneButtonPressed:(id)sender
+{
+}
+
+@end

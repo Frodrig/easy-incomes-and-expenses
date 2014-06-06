f12d3b31b6ab05e34d8d50ed552f9586039edbba
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-06-06 12:04:40 +0200

    Hemos añadido la capacidad de detener la animación flotante suavemente PERO hemos tenido que usar unas técnicas que han obligado a apoyarmnos en una CAKeyFrameAnimation. Esto ha hecho que no haya sido capaz de implementar la animación que quería y que consistía en una oscilación de extremo a extremo. Esta versión es una oscilación desde la posición inicial hasta arriba

--- Code/IncomesAndExpenses/UIView+FloatingAnimation.m
+++ Code/IncomesAndExpenses/UIView+FloatingAnimation.m
@@ -9,39 +9,78 @@
 #import "UIView+FloatingAnimation.h"
 
 static const CGFloat kYTranslationValue = 5.0;
-static NSString * const kAnimationKey = @"floatingAnimation";
+static const CGFloat kFloatingAnimationDuration = 0.75;
+static const CGFloat kEndSoftlyAnimationDuration = 0.5;
+static NSString * const kAnimationFloatingKey = @"floatingAnimation";
+static NSString * const kAnimationEndKey = @"endAnimation";
 
 @implementation UIView (FloatingAnimation)
 
 - (void)startFloatingAnimation
 {
     if (![self floatingAnimationActive]) {
-        [self.layer addAnimation:[self createFloatingBasicAnimation] forKey:kAnimationKey];
+        [self.layer addAnimation:[self createFloatingBasicAnimation] forKey:kAnimationFloatingKey];
+    } else {
+        NSLog(@"CANT START");
     }
 }
 
-- (CABasicAnimation *)createFloatingBasicAnimation
+- (CAAnimation *)createFloatingBasicAnimation
 {
-    CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"transform.translation.y"];
-    animation.removedOnCompletion = NO;
-    animation.autoreverses = YES;
-    animation.repeatCount = HUGE_VALF;
-    animation.toValue = @(kYTranslationValue);
-    animation.duration = 1;
-    animation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
-    animation.cumulative = NO;
-
-    return animation;
+    UIBezierPath *bezierPath = [[UIBezierPath alloc] init];
+    [bezierPath moveToPoint:CGPointMake(self.layer.position.x, self.layer.position.y)];
+    [bezierPath addLineToPoint:CGPointMake(self.layer.position.x, self.layer.position.y - kYTranslationValue)];
+    CAKeyframeAnimation *keyframeAnimation = [[CAKeyframeAnimation alloc] init];
+    keyframeAnimation.keyPath = @"position";
+    keyframeAnimation.path = bezierPath.CGPath;
+    keyframeAnimation.duration = kFloatingAnimationDuration;
+    keyframeAnimation.repeatCount = INFINITY;
+    keyframeAnimation.autoreverses = YES;
+    keyframeAnimation.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    
+    return keyframeAnimation;
 }
 
 - (BOOL)floatingAnimationActive
 {
-    return [self.layer animationForKey:kAnimationKey] != nil;
+    return [self.layer animationForKey:kAnimationFloatingKey] != nil || [self.layer animationForKey:kAnimationEndKey] != nil;
 }
 
 - (void)endCurrentFloatingAnimation
 {
-    [self.layer removeAnimationForKey:kAnimationKey];
+    if ([self.layer animationForKey:kAnimationFloatingKey]) {
+        [self.layer removeAnimationForKey:kAnimationFloatingKey];
+
+        CALayer *presentationLayer = (CALayer *)self.layer.presentationLayer;
+        CGPoint originalPosition = self.layer.position;
+        self.layer.position = presentationLayer.position;
+        
+        [self.layer addAnimation:[self createEndSoftlyAnimationFromOriginalPosition:originalPosition] forKey:kAnimationEndKey];
+    }
+}
+
+- (CAAnimation *)createEndSoftlyAnimationFromOriginalPosition:(CGPoint)originalPosition
+{
+    CABasicAnimation *endAnimationSoftly = [CABasicAnimation animationWithKeyPath:@"position"];
+    endAnimationSoftly.duration = kEndSoftlyAnimationDuration;
+    endAnimationSoftly.toValue = [NSValue valueWithCGPoint:originalPosition];
+    endAnimationSoftly.removedOnCompletion = NO;
+    endAnimationSoftly.fillMode = kCAFillModeForwards;
+    endAnimationSoftly.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseOut];
+    endAnimationSoftly.delegate = self;
+
+    return endAnimationSoftly;
+}
+
+- (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag
+{
+    if ([self.layer animationForKey:kAnimationEndKey] == anim) {
+        if (flag) {
+            CALayer *presentationLayer = (CALayer *)self.layer.presentationLayer;
+            self.layer.position = presentationLayer.position;
+            [self.layer removeAnimationForKey:kAnimationEndKey];
+        }
+    }
 }
 
 @end

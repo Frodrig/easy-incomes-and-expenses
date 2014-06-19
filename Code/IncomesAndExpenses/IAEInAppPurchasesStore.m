bbae7d6708294a83d7637a8435ec0b0cc7661708
Fernando Rodríguez Martínez <frodrig76@gmail.com>
2014-06-19 22:11:26 +0200

    wip

--- Code/IncomesAndExpenses/IAEInAppPurchasesStore.m
+++ Code/IncomesAndExpenses/IAEInAppPurchasesStore.m
@@ -13,7 +13,7 @@
 
 #pragma mark - Constants
 
-static NSString * const kInAppPurchaseProVersionIdentifier = @"proversion.easyincomesandexpenses.frodrig.com";
+static NSString * const kInAppPurchaseProVersionIdentifier = @"versionpro.easyincomesandexpenses.frodrig.com";
 
 #pragma mark - Definition
 
